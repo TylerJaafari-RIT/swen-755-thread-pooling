e0545b83794bcd4a3823216d71860c9763c2a9a9
JoJo Kaler <116104839+borebagger42@users.noreply.github.com>
2025-10-15 23:05:00 -0400

    added thread pooling

--- ThreadPooler.java
+++ ThreadPooler.java
@@ -5,22 +5,57 @@ import java.io.IOException;
 import java.io.Writer;
 import java.lang.Thread;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.HashMap;
+
+class RunnableThread implements Runnable{
+	private Map<String, String> wordMap;
+	private String fileName;
+	private String character;
+	private WordFinder wordFinder;
+
+	public RunnableThread(Map<String, String> wordMap, String fileName, String character){
+		this.wordMap = wordMap;
+		this.fileName = fileName;
+		this.character = character;
+		this.wordFinder = new WordFinder();
+	}
+
+	@Override
+	public void run() {
+		try{
+			wordFinder.getAllWords(wordMap, fileName, character);
+			
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("oopsie..");
+		}
+	}
+}
 
 public class ThreadPooler {
 	public static void main(String [] args) {
+		ExecutorService poolManager = Executors.newFixedThreadPool(10);
 		Map<String, String> wordMap = new HashMap<>();
-		WordFinder finder = new WordFinder();
 		String fileName = "bible.txt";
 		String outputFileName = "sorted.txt";
+		int totalThreadCalls = 0;
 
 		String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 		try {
 			long timer = System.currentTimeMillis();
-			for(char letter: alphabet.toCharArray()) {
-				finder.getAllWords(wordMap, fileName, String.valueOf(letter));
+			while (totalThreadCalls < alphabet.length()){
+				int threadsPooled = 0;
+				while((threadsPooled < 10) && totalThreadCalls<alphabet.length()){
+					poolManager.execute(new RunnableThread(wordMap, fileName,alphabet.substring(totalThreadCalls, totalThreadCalls+1)));
+					threadsPooled+=1;
+					totalThreadCalls+=1;
+					System.out.println(wordMap);
+				}
 			}
+			poolManager.shutdown();
 
 			File outputFile = new File(outputFileName);
 			Writer out = new FileWriter(outputFile);
